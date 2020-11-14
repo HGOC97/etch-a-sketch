309d0d12a2f7971c6aa65fe5e086d2698337141a
hgoc <ofirc36@gmail.com>
2020-11-14 13:30:40 +0200

    Moved from inline-block to grid layout, added changing grid size

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 const gridContainer = document.querySelector("#container");
+const gridReset = document.querySelector("#grid-edit");
+const gridSizeHint = document.querySelector("#grid-size-hint");
 
-// Create a 16x16 grid
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let cell = document.createElement("div");
-        cell.style["height"] = "32px";
-        cell.style["width"] = "32px";
-        cell.style["display"] = "inline-block";
-        cell.style["margin"] = "0 0 -4px 0";
-        cell.classList.add("cells");
-        gridContainer.appendChild(cell);
+createGrid(16);
+colorCell();
+
+function createGrid(gridSize) {
+    gridContainer.style["grid-template-columns"] = `repeat(${gridSize}, ${720/gridSize}px)`;
+    gridContainer.style["grid-template-rows"] = `repeat(${gridSize}, ${720/gridSize}px)`;
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cells");
+            gridContainer.appendChild(cell);
+        }
     }
 }
 
-const cells = document.querySelectorAll(".cells");
-
-// Change cell color on hover
-cells.forEach((cell) => {
-    cell.addEventListener("mouseover", function(e) {
-        let red = Math.random() * 255;
-        let green = Math.random() * 255;
-        let blue = Math.random() * 255;
-        e.target.style["background-color"] = `rgb(${red}, ${green}, ${blue})`
+function colorCell() {
+    let cells = document.querySelectorAll(".cells");
+    cells.forEach((cell) => {
+        cell.addEventListener("mouseover", function(e) {
+            let red = Math.random() * 255;
+            let green = Math.random() * 255;
+            let blue = Math.random() * 255;
+            e.target.style["background-color"] = `rgb(${red}, ${green}, ${blue})`
+        })
     })
+}
+
+gridReset.addEventListener("click", function(e) {
+    let newGridSize;
+    do {
+        newGridSize = parseInt(prompt("Enter new grid size - Min 8, Max 128"));
+    } while (!(newGridSize => 8) || !(newGridSize <= 128));
+    let cellsToRemove = document.querySelectorAll(".cells");
+    cellsToRemove.forEach((cell) => {
+        cell.remove();
+    })
+    gridSizeHint.textContent = `Current Grid: ${newGridSize}x${newGridSize}, Total ${newGridSize * newGridSize} cells`;
+    createGrid(newGridSize);
+    colorCell();
 })
